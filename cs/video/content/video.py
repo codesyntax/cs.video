1f6bfd2d04f0eba510ec3d0c038d6d0d64ca1b64
Mikel Larreategi <mlarreategi@codesyntax.com>
2010-01-15 08:06:09 +0000

    add AnnotationStorage to avoid Plone3 recursion errors

--- cs/video/content/video.py
+++ cs/video/content/video.py
@@ -19,6 +19,7 @@ VideoSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
     atapi.TextField('url',
                     required=1,
                     searchable=0,
+                    storage=atapi.AnnotationStorage(),
                     widget=atapi.StringWidget(label=_(u'URL'),
                                               description=_(u'Paste here the URL fo the flash video'),
                                               ),
