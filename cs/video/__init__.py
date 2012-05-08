d353b62dda950b76aaf7fdb722d333866728a8d0
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-05-08 08:24:18 +0000

    remove useless

--- cs/video/__init__.py
+++ cs/video/__init__.py
@@ -6,19 +6,12 @@ from cs.video import config
 
 from Products.Archetypes import atapi
 from Products.CMFCore import utils
-from Products.CMFCore.permissions import setDefaultRoles
 
 # Define a message factory for when this product is internationalised.
 # This will be imported with the special name "_" in most modules. Strings
 # like _(u"message") will then be extracted by i18n tools for translation.
 
 videoMessageFactory = MessageFactory('cs.video')
-
-from Products.CMFCore.DirectoryView import registerDirectory
-from config import GLOBALS
-
-registerDirectory('skins', GLOBALS)
-
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product.
--- cs/video/config.py
+++ cs/video/config.py
@@ -1,8 +1,6 @@
 """Common configuration constants
 """
 
-GLOBALS = globals()
-
 PROJECTNAME = 'cs.video'
 
 ADD_PERMISSIONS = {
@@ -10,16 +8,3 @@ ADD_PERMISSIONS = {
     'Video': 'cs.video: Add Video',
 }
 
-from Products.CMFPlone.utils import getFSVersionTuple
-
-version = getFSVersionTuple()
-
-if version[0] == 2:
-    IS_PLONE2 = True
-    IS_PLONE3 = False
-elif version[0] == 3:
-    IS_PLONE2 = False
-    IS_PLONE3 = True
-else: # What!!
-    IS_PLONE2 = False
-    IS_PLONE3 = False
